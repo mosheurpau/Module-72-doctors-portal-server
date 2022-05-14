e4a8b08cd67eb2fe775ba6ac64f0e9ab4a65b5a5
mosheurpau <mosheur.pau@gamil.com>
2022-05-14 15:34:55 +0600

    simple node mongo

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
@@ -6,6 +7,9 @@ const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
+
+const uri =
+  "mongodb+srv://doctor_admin:<password>@cluster0.lkqwj.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 
 app.get("/", (req, res) => {
   res.send("Hello From Doctor Portal!");
